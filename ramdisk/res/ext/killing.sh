330006079cccfd5c05c6f610e10ea4849027b114
javilonas <admin@lonasdigital.com>
2015-07-16 16:39:47 +0200

    Update killing.sh

--- ramdisk/res/ext/killing.sh
+++ ramdisk/res/ext/killing.sh
@@ -1,122 +1,143 @@
-#!/sbin/busybox sh
+#!/system/bin/sh
+#
+# Copyright (c) 2015 Javier Sayago <admin@lonasdigital.com>
+# Contact: javilonas@esp-desarrolladores.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 echo "............Closing APPS before init";
 
-/sbin/busybox killall -q kswapd0
+killall -q kswapd0
 sleep 71
 am kill-all
-/sbin/busybox killall -q com.android.MtpApplication
-/sbin/busybox killall -q com.sec.android.inputmethod
-/sbin/busybox killall -q com.sec.android.provider.logsprovider
-/sbin/busybox killall -q com.android.nfc
-/sbin/busybox killall -q com.samsung.android.providers.context
-/sbin/busybox killall -q com.sec.android.app.sysscope
-/sbin/busybox killall -q com.android.server.device.enterprise:remote
-/sbin/busybox killall -q com.android.server.vpn.enterprise:remote
-/sbin/busybox killall -q com.google.android.music:main
-/sbin/busybox killall -q com.google.android.gm
-/sbin/busybox killall -q com.google.process.gapps
-/sbin/busybox killall -q com.google.android.gsf.login
-/sbin/busybox killall -q com.google.android.apps.uploader
-/sbin/busybox killall -q android.process.media
-/sbin/busybox killall -q com.android.email
-/sbin/busybox killall -q com.sec.android.provider.badge
-/sbin/busybox killall -q com.google.android.youtube
-/sbin/busybox killall -q com.android.calendar
-/sbin/busybox killall -q com.android.providers.calendar
-/sbin/busybox killall -q com.android.mms
-/sbin/busybox killall -q com.wsomacp
-/sbin/busybox killall -q com.samsung.map
-/sbin/busybox killall -q org.simalliance.openmobileapi.service:remote
-/sbin/busybox killall -q com.google.android.apps.maps:FriendService
-/sbin/busybox killall -q com.google.android.apps.maps:GoogleLocationService
-/sbin/busybox killall -q eu.chainfire.supersu
-/sbin/busybox killall -q com.google.android.partnersetup
-/sbin/busybox killall -q com.sec.android.app.controlpanel
-/sbin/busybox killall -q com.android.contacts
-/sbin/busybox killall -q com.google.android.googlequicksearchbox
-/sbin/busybox killall -q com.sec.android.app.tmserver
-/sbin/busybox killall -q com.google.android.apps.maps
-/sbin/busybox killall -q com.sec.android.gallery3d
-/sbin/busybox killall -q com.sec.android.app.videoplayer
-/sbin/busybox killall -q com.google.android.apps.maps:LocationFriendService
-/sbin/busybox killall -q com.sec.android.app.fm
-/sbin/busybox killall -q com.android.music
-/sbin/busybox killall -q com.android.browser
-/sbin/busybox killall -q com.osp.app.signin
-/sbin/busybox killall -q com.samsung.bt.avrcp
-/sbin/busybox killall -q com.broadcom.bt.app.system
-/sbin/busybox killall -q com.sec.android.providers.drm
-/sbin/busybox killall -q com.seven.Z7.service
-/sbin/busybox killall -q com.android.MtpApplication
-/sbin/busybox killall -q org.projectvoodoo.louder
-/sbin/busybox killall -q com.google.android.apps.maps:LocationFriendService
-/sbin/busybox killall -q android.process.media
-/sbin/busybox killall -q com.android.email
-/sbin/busybox killall -q com.sec.android.app.twdvfs
-/sbin/busybox killall -q com.android.server.vpn.enterprise:remote
-/sbin/busybox killall -q com.android.contacts
-/sbin/busybox killall -q com.samsung.map
-/sbin/busybox killall -q com.sec.android.app.tmserver
-/sbin/busybox killall -q com.sec.factory
-/sbin/busybox killall -q com.google.android.youtube
-/sbin/busybox killall -q com.fmm.dm
-/sbin/busybox killall -q com.google.android.apps.uploader
-/sbin/busybox killall -q com.sec.android.app.popupuireceiver
-/sbin/busybox killall -q com.google.android.gsf.login
-/sbin/busybox killall -q com.sec.android.gallery3d
-/sbin/busybox killall -q com.sec.android.widgetapp.alarmclock
-/sbin/busybox killall -q org.simalliance.openmobileapi.service:remote
-/sbin/busybox killall -q com.google.process.gapps
-/sbin/busybox killall -q com.google.android.apps.maps:Netwo
-/sbin/busybox killall -q com.google.android.apps.maps:Frien
-/sbin/busybox killall -q com.sec.android.app.samsungapps.una
-/sbin/busybox killall -q com.google.process.gapps
-/sbin/busybox killall -q com.sec.android.widgetapp.apnews
-/sbin/busybox killall -q com.android.email
-/sbin/busybox killall -q com.sec.android.app.controlpanel
-/sbin/busybox killall -q com.cooliris.media
-/sbin/busybox killall -q com.qo.android.am3
-/sbin/busybox killall -q com.qo.android.am3:remote
-/sbin/busybox killall -q com.android.updater
-/sbin/busybox killall -q com.google.android.partnersetup
-/sbin/busybox killall -q com.google.android.gm
-/sbin/busybox killall -q com.android.vending
-/sbin/busybox killall -q com.google.android.voicesearch
-/sbin/busybox killall -q com.google.android.googlequicksearchbox
-/sbin/busybox killall -q com.android.voicedialer
-/sbin/busybox killall -q android.process.media
-/sbin/busybox killall -q com.android.mms
-/sbin/busybox killall -q com.android.calendar
-/sbin/busybox killall -q com.android.providers.calendar
-/sbin/busybox killall -q com.sec.android.Kies
-/sbin/busybox killall -q com.sec.android.app.ve
-/sbin/busybox killall -q com.google.process.gapps
-/sbin/busybox killall -q com.sec.android.providers.downloads
-/sbin/busybox killall -q com.sec.android.daemonapp.accuweather
-/sbin/busybox killall -q com.sec.android.fotaclient
-/sbin/busybox killall -q com.android.deskclock
-/sbin/busybox killall -q com.android.bluetooth
-/sbin/busybox killall -q com.google.android.apps.uploader
-/sbin/busybox killall -q com.google.android.apps.maps:FriendService
-/sbin/busybox killall -q com.google.android.gm
-/sbin/busybox killall -q com.android.im
-/sbin/busybox killall -q android.process.im
-/sbin/busybox killall -q alei.switchpro
-/sbin/busybox renice -20 `pidof com.android.phone`
-/sbin/busybox renice -14 `pidof com.gau.go.launcherex`
-/sbin/busybox renice -14 `pidof com.android.launcher2`
-/sbin/busybox renice -14 `pidof com.teslacoilsw.launcher`
-/sbin/busybox renice -14 `pidof com.anddoes.launcher`
-/sbin/busybox renice -14 `pidof com.sec.android.app.launcher`
-/sbin/busybox renice -14 `pidof com.android.launcher`
-/sbin/busybox renice -14 `pidof com.teslacoilsw.launcher`
-/sbin/busybox renice -14 `pidof com.teslacoilsw.launcher.prime`
-/sbin/busybox renice -14 `pidof org.adw.launcher`
-/sbin/busybox renice -15 `pidof com.android.systemui`
-/sbin/busybox renice -9 `pidof com.android.settings`
-/sbin/busybox renice -3 `pidof android.process.acore`
-/sbin/busybox renice -3 `pidof android.process.media`
+killall -q com.android.MtpApplication
+killall -q com.sec.android.inputmethod
+killall -q com.sec.android.provider.logsprovider
+killall -q com.android.nfc
+killall -q com.samsung.android.providers.context
+killall -q com.sec.android.app.sysscope
+killall -q com.android.server.device.enterprise:remote
+killall -q com.android.server.vpn.enterprise:remote
+killall -q com.google.android.music:main
+killall -q com.google.android.gm
+killall -q com.google.process.gapps
+killall -q com.google.android.gsf.login
+killall -q com.google.android.apps.uploader
+killall -q android.process.media
+killall -q com.android.email
+killall -q com.sec.android.provider.badge
+killall -q com.google.android.youtube
+killall -q com.android.calendar
+killall -q com.android.providers.calendar
+killall -q com.android.mms
+killall -q com.wsomacp
+killall -q com.samsung.map
+killall -q org.simalliance.openmobileapi.service:remote
+killall -q com.google.android.apps.maps:FriendService
+killall -q com.google.android.apps.maps:GoogleLocationService
+killall -q eu.chainfire.supersu
+killall -q com.google.android.partnersetup
+killall -q com.sec.android.app.controlpanel
+killall -q com.android.contacts
+killall -q com.google.android.googlequicksearchbox
+killall -q com.sec.android.app.tmserver
+killall -q com.google.android.apps.maps
+killall -q com.sec.android.gallery3d
+killall -q com.sec.android.app.videoplayer
+killall -q com.google.android.apps.maps:LocationFriendService
+killall -q com.sec.android.app.fm
+killall -q com.android.music
+killall -q com.android.browser
+killall -q com.osp.app.signin
+killall -q com.samsung.bt.avrcp
+killall -q com.broadcom.bt.app.system
+killall -q com.sec.android.providers.drm
+killall -q com.seven.Z7.service
+killall -q com.android.MtpApplication
+killall -q org.projectvoodoo.louder
+killall -q com.google.android.apps.maps:LocationFriendService
+killall -q android.process.media
+killall -q com.android.email
+killall -q com.sec.android.app.twdvfs
+killall -q com.android.server.vpn.enterprise:remote
+killall -q com.android.contacts
+killall -q com.samsung.map
+killall -q com.sec.android.app.tmserver
+killall -q com.sec.factory
+killall -q com.google.android.youtube
+killall -q com.fmm.dm
+killall -q com.google.android.apps.uploader
+killall -q com.sec.android.app.popupuireceiver
+killall -q com.google.android.gsf.login
+killall -q com.sec.android.gallery3d
+killall -q com.sec.android.widgetapp.alarmclock
+killall -q org.simalliance.openmobileapi.service:remote
+killall -q com.google.process.gapps
+killall -q com.google.android.apps.maps:Netwo
+killall -q com.google.android.apps.maps:Frien
+killall -q com.sec.android.app.samsungapps.una
+killall -q com.google.process.gapps
+killall -q com.sec.android.widgetapp.apnews
+killall -q com.android.email
+killall -q com.sec.android.app.controlpanel
+killall -q com.cooliris.media
+killall -q com.qo.android.am3
+killall -q com.qo.android.am3:remote
+killall -q com.android.updater
+killall -q com.google.android.partnersetup
+killall -q com.google.android.gm
+killall -q com.android.vending
+killall -q com.google.android.voicesearch
+killall -q com.google.android.googlequicksearchbox
+killall -q com.android.voicedialer
+killall -q android.process.media
+killall -q mediaserver
+killall -q com.android.mms
+killall -q com.android.calendar
+killall -q com.android.providers.calendar
+killall -q com.sec.android.Kies
+killall -q com.sec.android.app.ve
+killall -q com.google.process.gapps
+killall -q com.sec.android.providers.downloads
+killall -q com.sec.android.daemonapp.accuweather
+killall -q com.sec.android.fotaclient
+killall -q com.android.deskclock
+killall -q com.android.bluetooth
+killall -q com.google.android.apps.uploader
+killall -q com.google.android.apps.maps:FriendService
+killall -q com.google.android.gm
+killall -q com.android.im
+killall -q android.process.im
+killall -q alei.switchpro
+renice -20 `pidof com.android.phone`
+renice -14 `pidof com.gau.go.launcherex`
+renice -14 `pidof com.android.launcher2`
+renice -14 `pidof com.teslacoilsw.launcher`
+renice -14 `pidof com.anddoes.launcher`
+renice -14 `pidof com.sec.android.app.launcher`
+renice -14 `pidof com.android.launcher`
+renice -14 `pidof com.teslacoilsw.launcher`
+renice -14 `pidof com.teslacoilsw.launcher.prime`
+renice -14 `pidof org.adw.launcher`
+renice -15 `pidof com.android.systemui`
+renice -9 `pidof com.android.settings`
+renice -3 `pidof android.process.acore`
+renice -3 `pidof android.process.media`
+
+if [ "`pgrep media`" ] && [ "`pgrep mediaserver`" ]; then
+	killall -9 android.process.media
+	killall -9 mediaserver
+fi
 
 echo "............done";
