7e1603cabbedd99b942184fd912b043451422cae
Pafcholini <nadyaivanova14@gmail.com>
2016-03-03 11:50:34 +0100

    cpu: keep 2 cores always online Core 0 and 1 are always online. mpdecision supposed to keep 2 cores always online but after 5.1.1 update is not keeping them.
    Thanks to andip71 for the code!
    
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -371,6 +371,13 @@ int __ref cpu_down(unsigned int cpu)
 
 	cpu_maps_update_begin();
 
+	// Keep CPU cores 0 and 1 always on
+	if ((cpu == 0) || (cpu == 1))
+	{
+		err = -EBUSY;
+		goto out;
+	}
+
 	if (cpu_hotplug_disabled) {
 		err = -EBUSY;
 		goto out;
