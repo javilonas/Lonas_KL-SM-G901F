642f4f59f6370067c0f03cbf8413dbed37f568e7
javilonas <admin@lonasdigital.com>
2016-03-11 16:59:57 +0100

    Update wakeup

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -15,8 +15,8 @@
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
 #include <linux/types.h>
-#include <linux/moduleparam.h>
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
 
 #include "power.h"
 
