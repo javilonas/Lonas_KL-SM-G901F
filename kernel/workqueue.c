de7c610794d22ac921d6021ae30e4575b89468d8
javilonas <admin@lonasdigital.com>
2016-03-19 11:23:41 +0100

    Revert "Fix redeclaration"
    This reverts commit 19524b12270f1056ef45067f1ede573106bdbbcb.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -282,6 +282,15 @@ static bool wq_power_efficient;
 
 module_param_named(power_efficient, wq_power_efficient, bool, 0444);
 
+/* see the comment above the definition of WQ_POWER_EFFICIENT */
+#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+static bool wq_power_efficient = true;
+#else
+static bool wq_power_efficient;
+#endif
+
+module_param_named(power_efficient, wq_power_efficient, bool, 0444);
+
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 
 /* buf for wq_update_unbound_numa_attrs(), protected by CPU hotplug exclusion */
